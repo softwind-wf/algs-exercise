9f5fafc44f529f4fdbd0a217b5694bded3fa0e49
softWind <1539196513@qq.com>
2026-05-22 02:05:28 +0800

    2026/5/22 2:05提交

--- src/main/java/test4/test4_4/TestDijkstra.java
+++ src/main/java/test4/test4_4/TestDijkstra.java
@@ -5,26 +5,15 @@ import java.util.Stack;
 public class TestDijkstra {
     public static void main(String[] args) {
         // 构建一个有向加权图
-        int V = 8;
+        int V = 4;
         EdgeWeightedDigraph G = new EdgeWeightedDigraph(V);
 
         // 添加边
-        G.addEdge(new DirectedEdge(4, 5, 0.35));
-        G.addEdge(new DirectedEdge(5, 4, 0.35));
-        G.addEdge(new DirectedEdge(4, 7, 0.37));
-        G.addEdge(new DirectedEdge(5, 7, 0.28));
-        G.addEdge(new DirectedEdge(7, 5, 0.28));
-        G.addEdge(new DirectedEdge(5, 1, 0.32));
-        G.addEdge(new DirectedEdge(0, 4, 0.38));
-        G.addEdge(new DirectedEdge(0, 2, 0.26));
-        G.addEdge(new DirectedEdge(7, 3, 0.39));
-        G.addEdge(new DirectedEdge(1, 3, 0.29));
-        G.addEdge(new DirectedEdge(2, 7, 0.34));
-        G.addEdge(new DirectedEdge(6, 2, 0.40));
-        G.addEdge(new DirectedEdge(6, 0, 0.58));
-        G.addEdge(new DirectedEdge(3, 6, 0.52));
-        G.addEdge(new DirectedEdge(1, 2, 0.36));
-        G.addEdge(new DirectedEdge(6, 4, 0.93));
+        G.addEdge(new DirectedEdge(0, 1, 2));
+        G.addEdge(new DirectedEdge(0, 2, 5));
+        G.addEdge(new DirectedEdge(2, 1, -4));
+        G.addEdge(new DirectedEdge(1, 3, 1));
+
 
         // 从起点0开始计算最短路径
         int s = 0;
